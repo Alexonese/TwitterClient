7ffd7fb2886e93fc417f2b67b8a88b09af882f9a
Alexonese <34609133+Alexonese@users.noreply.github.com>
2019-01-03 02:42:16 +0200

    Создание POJO объекта User

--- app/src/main/java/todofire/alexo/com/mytweeetlearning/UserInfoActivity.java
+++ app/src/main/java/todofire/alexo/com/mytweeetlearning/UserInfoActivity.java
@@ -3,20 +3,64 @@ package todofire.alexo.com.mytweeetlearning;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.squareup.picasso.Picasso;
+
+import todofire.alexo.com.mytweeetlearning.pojo.User;
 
 public class UserInfoActivity extends AppCompatActivity {
 
     private ImageView userImageView;
-
+    private TextView nameTextView;
+    private TextView  nickTextView;
+    private TextView  descriptionTextView;
+    private TextView  locationTextView;
+    private TextView followingCountTextView;
+    private TextView followersCountTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
         userImageView = findViewById(R.id.user_image_view);
-        Picasso.get().load("http://i.imgur.com/DvpvklR.png").into(userImageView);
+        userImageView = findViewById(R.id.user_image_view);
+        nameTextView = findViewById(R.id.user_name_text_view);
+        nickTextView = findViewById(R.id.user_nick_text_view);
+        descriptionTextView = findViewById(R.id.user_description_text_view);
+        locationTextView = findViewById(R.id.user_location_text_view);
+        followingCountTextView = findViewById(R.id.following_count_text_view);
+        followersCountTextView = findViewById(R.id.followers_count_text_view);
+
+
+        loadUserInfo();
+    }
+
+    private void displayUserInfo(User user){
+        Picasso.get().load(user.getImageUrl()).into(userImageView);
+
+        nameTextView.setText(user.getName());
+        nickTextView.setText(user.getNick());
+        descriptionTextView.setText(user.getDescription());
+        locationTextView.setText(user.getLocation());
+        String followingCount = String.valueOf(user.getFollowingCount());
+        followingCountTextView.setText(followingCount);
+        String followersCount = String.valueOf(user.getFollowersCount());
+        followersCountTextView.setText(followersCount);
+
 
     }
+
+    private void loadUserInfo(){
+
+        User user = getUser();
+        displayUserInfo(user);
+    }
+
+
+        private User getUser(){
+            return new User(1L, "http://i.imgur.com/DvpvklR.png", "Alexonese", "@alexonese" , "Sample text", "Bolgaria", 36, 26 );
+
+        }
+
 
 }
